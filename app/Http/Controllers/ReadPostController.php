dc61377e3d3bc7fe8460e704501b74f2810ab580
muhammadusamayounas <muhammad_osama@ucp.edu.pk>
2021-11-25 12:55:14 +0500

    projectlaravel

--- app/Http/Controllers/ReadPostController.php
+++ app/Http/Controllers/ReadPostController.php
@@ -6,7 +6,7 @@ use App\Http\Requests\LoginAccessRequest;
 use App\Models\post;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
-
+use App\Http\Resources\postresource;
 class ReadPostController extends Controller
 {
 
@@ -14,7 +14,8 @@ class ReadPostController extends Controller
     {
           $id=$request->data[0]->id;//geting user id
           $data = DB::table('posts')->where('user_id', $id)->get();//query to read post data on the bases of user id 
-          return response()->json(['message'=>$data]);
+          return new postresource($data);
+          //response()->json(['message'=>$data]);
     }
 
 }
